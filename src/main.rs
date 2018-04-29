5a243b8262abecc11f042adf8007e21ff7dde5be
Alexey Voznyuk <me@swizard.info>
2018-04-30 00:02:42 +0300

    initial skeleton commit

---
+++ src/main.rs
@@ -0,0 +1,111 @@
+extern crate rtt;
+extern crate gfx_core;
+extern crate env_logger;
+extern crate piston_window;
+#[macro_use] extern crate log;
+#[macro_use] extern crate clap;
+
+use std::{io, process};
+use std::path::PathBuf;
+
+use clap::Arg;
+use piston_window::{
+    OpenGL,
+    PistonWindow,
+    WindowSettings,
+    TextureSettings,
+    // Viewport,
+    Glyphs,
+    PressEvent,
+    Button,
+    Key
+};
+
+fn main() {
+    env_logger::init();
+    match run() {
+        Ok(()) =>
+            info!("graceful shutdown"),
+        Err(e) => {
+            error!("fatal error: {:?}", e);
+            process::exit(1);
+        },
+    }
+}
+
+#[derive(Debug)]
+enum Error {
+    MissingParameter(&'static str),
+    Piston(PistonError),
+}
+
+#[derive(Debug)]
+enum PistonError {
+    BuildWindow(String),
+    LoadFont { file: String, error: io::Error, },
+    DrawText(gfx_core::factory::CombinedError),
+}
+
+const CONSOLE_HEIGHT: u32 = 32;
+const BORDER_WIDTH: u32 = 16;
+const SCREEN_WIDTH: u32 = 640;
+const SCREEN_HEIGHT: u32 = 480;
+
+fn run() -> Result<(), Error> {
+    let matches = app_from_crate!()
+        .arg(Arg::with_name("assets-dir")
+             .short("a")
+             .long("assets-dir")
+             .value_name("DIR")
+             .help("Graphics resources directory")
+             .default_value("./assets")
+             .takes_value(true))
+        .get_matches();
+
+    let assets_dir = matches.value_of("assets-dir")
+        .ok_or(Error::MissingParameter("assets-dir"))?;
+
+    let opengl = OpenGL::V3_2;
+    let mut window: PistonWindow = WindowSettings::new("RTT demo", [SCREEN_WIDTH, SCREEN_HEIGHT])
+        .exit_on_esc(true)
+        .opengl(opengl)
+        .build()
+        .map_err(PistonError::BuildWindow)
+        .map_err(Error::Piston)?;
+
+    let mut font_path = PathBuf::from(assets_dir);
+    font_path.push("FiraSans-Regular.ttf");
+    let mut glyphs = Glyphs::new(&font_path, window.factory.clone(), TextureSettings::new())
+        .map_err(|e| Error::Piston(PistonError::LoadFont {
+            file: font_path.to_string_lossy().to_string(),
+            error: e,
+        }))?;
+
+    while let Some(event) = window.next() {
+        let maybe_result = window.draw_2d(&event, |context, g2d| {
+            use piston_window::{clear, text, Transformed};
+            clear([0.0, 0.0, 0.0, 1.0], g2d);
+            text::Text::new_color([0.0, 1.0, 0.0, 1.0], 16).draw(
+                &format!("Mode: [ path ]; press <M> to switch mode, <C> to clear or <Q> to exit"),
+                &mut glyphs,
+                &context.draw_state,
+                context.transform.trans(5.0, 20.0),
+                g2d
+            ).map_err(PistonError::DrawText)?;
+
+            Ok(())
+        });
+        if let Some(result) = maybe_result {
+            let () = result.map_err(Error::Piston)?;
+        }
+
+        match event.press_args() {
+            Some(Button::Keyboard(Key::Q)) =>
+                break,
+            _ =>
+                (),
+        }
+    }
+
+    Ok(())
+}
